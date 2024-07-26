cffda923c70acfde5905218f26876304960028b8
Chris F <CF-public@gmx.net>
2024-07-26 17:22:16 +0200

    Added Harmony patch to allow administering Healer Mech Serums to animals

--- Source/HarmonyPatches.cs
+++ Source/HarmonyPatches.cs
@@ -24,6 +24,10 @@ namespace HMSChoice
 				typeof(CompUsable).GetMethod("UsedBy"),
 				prefix: new HarmonyMethod(typeof(HarmonyPatches), nameof(CompUsable_UsedBy_Prefix)));
 
+			harmony.Patch(
+				typeof(CompUsable).GetMethod("CanBeUsedBy"),
+				transpiler: new HarmonyMethod(typeof(HarmonyPatches), nameof(CompUsable_CanBeUsedBy_Transpiler)));
+
 
 			// compatibility patch for More Faction Interaction's Mystical Shaman healer
 			var methodInfo_MysticalShaman_Notify_CaravanArrived = Type.GetType("MoreFactionInteraction.More_Flavour.MysticalShaman, MoreFactionInteraction")?.GetMethod("Notify_CaravanArrived");
@@ -31,7 +35,7 @@ namespace HMSChoice
 			{
 				harmony.Patch(
 					methodInfo_MysticalShaman_Notify_CaravanArrived,
-					transpiler: new HarmonyMethod(typeof(MFI_MysticalShaman_Patcher), nameof(MFI_MysticalShaman_Patcher.Notify_CaravanArrived_Transpiler)));
+					transpiler: new HarmonyMethod(typeof(HarmonyPatches), nameof(Notify_CaravanArrived_Transpiler)));
 			}
 		}
 
@@ -44,12 +48,47 @@ namespace HMSChoice
 			}
 			return true;
 		}
-	}
 
+		internal static IEnumerable<CodeInstruction> CompUsable_CanBeUsedBy_Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+		{
+			var list = instructions.ToList();
 
-	// patcher for More Faction Interaction's Mystical Shaman healer
-	internal class MFI_MysticalShaman_Patcher
-	{
+			var success = false;
+			Label? label0 = null;
+			var label1 = generator.DefineLabel();
+			for (int i = 0; i < list.Count; i++)
+			{
+				if (label0 == null)
+				{
+					if (list[i].opcode == OpCodes.Callvirt && list[i].operand is MethodInfo mi0 && mi0.Name == "get_Humanlike"
+						&& list[i + 1].opcode == OpCodes.Brtrue_S)
+					{
+						label0 = list[++i].labels?.FirstOrDefault();
+
+						list.Insert(++i, new CodeInstruction(OpCodes.Ldtoken, typeof(CompUsable_FixHealthConditionChoice)));
+						list.Insert(++i, new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(Type), nameof(Type.GetTypeFromHandle))));
+						list.Insert(++i, new CodeInstruction(OpCodes.Ldarg_0));
+						list.Insert(++i, new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(object), nameof(GetType))));
+						list.Insert(++i, new CodeInstruction(OpCodes.Callvirt, AccessTools.Method(typeof(Type), nameof(Type.IsAssignableFrom))));
+						list.Insert(++i, new CodeInstruction(OpCodes.Brtrue_S, label1));
+					}
+				}
+				else
+				{
+					if (list[i].labels.Contains((Label)label0))
+					{
+						list[i].labels.Add(label1);
+						success = true;
+						break;
+					}
+				}
+			}
+			if (!success)
+				Log.Error($"{nameof(HMSChoice)}: applying CanBeUsedBy patch failed");
+
+			return list;
+		}
+
 		internal static IEnumerable<CodeInstruction> Notify_CaravanArrived_Transpiler(IEnumerable<CodeInstruction> instructions)
 		{
 			bool replace = false;
@@ -77,7 +116,7 @@ namespace HMSChoice
 						&& methodInfo.Name == "DoEffect")
 					{
 						instruction.opcode = OpCodes.Call;
-						instruction.operand = typeof(MFI_MysticalShaman_Patcher).GetMethod(nameof(MechSerumHealer_FixWorstHealthCondition), BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public);
+						instruction.operand = AccessTools.Method(typeof(HarmonyPatches), nameof(MechSerumHealer_FixWorstHealthCondition));
 						replace = false;
 					}
 					else // skip instruction
